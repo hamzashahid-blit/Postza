2fc312918af750e7868f5ef423559ebb4bb9cf78
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2022-08-20 11:05:34 +0500

    Fixed CORS issue

--- src/routes/Request.svelte
+++ src/routes/Request.svelte
@@ -4,6 +4,7 @@
   import { headers, queryParams, rawResponse, timeTaken, jsonData } from "./stores";
   import TabButtons from "./TabButtons.svelte";
   import Tabs from "./Tabs.svelte";
+import { npm_package_scripts_package } from "$env/static/private";
 
   let method: string, url: string;
   let startTime: number;
@@ -17,14 +18,17 @@
   }
 
   function send(_: CustomEvent) {
+	// Add proxy to send request to URL, add Access-Control-Allow-Origin to send to us.
+	// This also solves the issue of bad errors as api.allorigins.win handles those.
+	let proxiedUrl = "https://api.allorigins.win/get?url=" + encodeURIComponent(url);
 	axios({
-      url: url,
+      url: proxiedUrl,
   	  method: method,
 	  params: storeToObject($queryParams),
 	  headers: storeToObject($headers),
 	  data: $jsonData
 	}).catch(e => e) // Handle 404
-	  .then((response: AxiosResponse) => rawResponse.set(response));
+	  .then((res: AxiosResponse) => rawResponse.set(res));
   }
 
   axios.interceptors.request.use(
